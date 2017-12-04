4b42bf54c07d355a730215054e0f1ec87db30f91
aszlig <aszlig@nix.build>
2017-12-04 11:20:20 +0100

    Switch to using the hyphenation package
    The hyphenation package already has a large set of different hyphenation
    rules included for different languages, so it's really a bit redudant if
    we duplicate the effort for HPDF.
    
    Right now this implementation doesn't use the hyphenation package to its
    full extent, because we still have a hardcoded language list, which in
    this case is German (which I added) and English.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- Graphics/PDF/Typesetting/WritingSystem.hs
+++ Graphics/PDF/Typesetting/WritingSystem.hs
@@ -17,13 +17,14 @@ module Graphics.PDF.Typesetting.WritingSystem(
 
 import qualified Data.Text as T
 import Graphics.PDF.LowLevel.Types
-import qualified Graphics.PDF.Hyphenate as H
+import qualified Text.Hyphenation as H
 import Data.List(intersperse)
 import Data.Char 
 import Data.List(unfoldr)
 
-data Language = English 
-              | OtherLanguage 
+data Language = English
+              | German
+              | OtherLanguage
 
 data WritingSystem = Latin Language
                    | UnknownWritingSystem
@@ -49,9 +50,26 @@ myWords l = concatMap onlyWord . unfoldr myWords' $ l
          (True,False) -> [p]
          (False,False) -> [w,p]
 
-addHyphens :: H.HyphenationDatabase -> T.Text -> T.Text
-addHyphens db f = T.concat . map (T.concat . intersperse (T.pack "/-") . H.hyphenate db) . myWords $ f
+addHyphens :: H.Hyphenator -> T.Text -> T.Text
+addHyphens hn f =
+    T.concat . map (T.concat . intersperse (T.pack "/-") . hyphenate) . myWords $ f
+  where
+    hyphenate = fmap T.pack . H.hyphenate hn . T.unpack
 
+
+mapGlyphsGerEng :: H.Hyphenator -> T.Text -> [SpecialChar]
+mapGlyphsGerEng hn theText =
+  let getBreakingGlyphs [] = []
+      getBreakingGlyphs (a:'/':'-':d:l) = (NormalChar a):BreakingHyphen:getBreakingGlyphs (d:l)
+      getBreakingGlyphs (',':' ':l) = NormalChar ',':BiggerSpace:getBreakingGlyphs l
+      getBreakingGlyphs (';':' ':l) = NormalChar ';':BiggerSpace:getBreakingGlyphs l
+      getBreakingGlyphs ('.':' ':l) = NormalChar '.':BiggerSpace:getBreakingGlyphs l
+      getBreakingGlyphs (':':' ':l) = NormalChar ':':BiggerSpace:getBreakingGlyphs l
+      getBreakingGlyphs ('!':' ':l) = NormalChar '!':BiggerSpace:getBreakingGlyphs l
+      getBreakingGlyphs ('?':' ':l) = NormalChar '?':BiggerSpace:getBreakingGlyphs l
+      getBreakingGlyphs (' ':l) = NormalSpace:getBreakingGlyphs l
+      getBreakingGlyphs (a:l) = NormalChar a:getBreakingGlyphs l
+  in getBreakingGlyphs (T.unpack . addHyphens hn $ theText)
 
 mapToSpecialGlyphs :: WritingSystem -> T.Text -> [SpecialChar]
 mapToSpecialGlyphs UnknownWritingSystem theText = 
@@ -64,15 +82,5 @@ mapToSpecialGlyphs (Latin OtherLanguage) theText =
       getBreakingGlyphs (a:l) = NormalChar a:getBreakingGlyphs l 
       getBreakingGlyphs [] = []
   in getBreakingGlyphs (T.unpack theText)
-mapToSpecialGlyphs (Latin English) theText = 
-  let getBreakingGlyphs [] = []
-      getBreakingGlyphs (a:'/':'-':d:l) = (NormalChar a):BreakingHyphen:getBreakingGlyphs (d:l)
-      getBreakingGlyphs (',':' ':l) = NormalChar ',':BiggerSpace:getBreakingGlyphs l
-      getBreakingGlyphs (';':' ':l) = NormalChar ';':BiggerSpace:getBreakingGlyphs l
-      getBreakingGlyphs ('.':' ':l) = NormalChar '.':BiggerSpace:getBreakingGlyphs l
-      getBreakingGlyphs (':':' ':l) = NormalChar ':':BiggerSpace:getBreakingGlyphs l
-      getBreakingGlyphs ('!':' ':l) = NormalChar '!':BiggerSpace:getBreakingGlyphs l
-      getBreakingGlyphs ('?':' ':l) = NormalChar '?':BiggerSpace:getBreakingGlyphs l
-      getBreakingGlyphs (' ':l) = NormalSpace:getBreakingGlyphs l
-      getBreakingGlyphs (a:l) = NormalChar a:getBreakingGlyphs l
-  in getBreakingGlyphs (T.unpack . addHyphens (H.English Nothing) $ theText)
+mapToSpecialGlyphs (Latin German) theText = mapGlyphsGerEng H.german_1996 theText
+mapToSpecialGlyphs (Latin English) theText = mapGlyphsGerEng H.english_US theText
